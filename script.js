ac7f2ed4a0b0ce622da8ecf9468cea84126546dc
John Dungao <johnmiguel.dungao@gmail.com>
2023-09-25 20:12:12 -0400

    prevent default of form submit, display a success message when user click submit. add button on home on success message

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+let contactForm = document.querySelector(".contact-form");
+let contactContainer = document.querySelector(".contact-container");
+// let contactBtn = document.querySelector("#submit-btn");
+
+console.log(contactForm);
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  contactContainer.classList.add("hidden");
+  document.querySelector(".success").classList.remove("hidden");
+});
